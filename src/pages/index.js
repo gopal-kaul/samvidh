f464993e14c1ec4c17fd885f7218000816ecedd8
Gopal Kaul <gopalkaul5@gmail.com>
2023-01-19 20:23:37 +0530

    Add responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className="px-[10%] font-graphik pt-[5%]">
-        <div className="flex flex-col-reverse md:flex-row gap-20">
+        <div className="flex flex-col md:flex-row gap-20">
           <div className="relative overflow-clip md:w-1/3 w-full">
             <Image
               src={person}
@@ -26,12 +26,12 @@ export default function Home() {
             />
           </div>
           <div className="animate-slideContent translate-y-[20%] w-full md:w-2/3 flex flex-col justify-between">
-            <h2 className="lg:text-[3.7vw] font-semibold leading-tight">
+            <h2 className="text-[8vw] md:text-[4vw] lg:text-[3.7vw] font-semibold leading-tight">
               The Experts in
               <br />
               Mulesoft Integration
             </h2>
-            <p className="lg:text-[1.8vw] leading-tight">
+            <p className="pt-[10%] pb-[15%] md:py-0 text-[4vw] md:text-[2vw] lg:text-[1.8vw] leading-tight">
               Samvidh is the premier choice for Mulesoft services, offering a
               comprehensive range of integration and API management solutions
               that will help you to transform your business and drive digital
